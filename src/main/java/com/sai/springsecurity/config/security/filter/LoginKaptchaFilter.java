7919031589e33f190a4c49df1f83a20c00b85056
SAI <2932109991@qq.com>
2023-06-18 19:58:44 +0800

    [init] 验证码v2

--- src/main/java/com/sai/springsecurity/config/security/filter/LoginKaptchaFilter.java
+++ src/main/java/com/sai/springsecurity/config/security/filter/LoginKaptchaFilter.java
@@ -1,8 +1,15 @@
 package com.sai.springsecurity.config.security.filter;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * @author SAI
  * @create 2023-06-18
  */
-public class LoginKaptchaFilter {
+public class LoginKaptchaFilter extends GenericFilter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
 }
